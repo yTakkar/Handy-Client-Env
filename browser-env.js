1729f260eadd7da2bc4af67239c97dc6a60d03f1
yTakkar <www.shtakkar@gmail.com>
2018-02-25 22:21:12 +0530

    Deleted else stmt

--- browser-env.js
+++ browser-env.js
@@ -2,7 +2,7 @@
 
 const
   fs = require('fs'),
-  { success, error } = require('handy-log'),
+  { success } = require('handy-log'),
   { promisify } = require('util'),
   { coroutine } = require('bluebird'),
   append = promisify(fs.appendFile),
@@ -19,9 +19,6 @@ coroutine(function* (){
 
   if (fileExists) {
     deleteFile(outputFile)
-  } else {
-    error('.Env file is missing in this directory!!')
-    return
   }
 
   for (const elem in valuesFromFile) {
