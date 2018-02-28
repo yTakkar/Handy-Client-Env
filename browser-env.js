ca3660d3c4d7f4abf7c1540b4a283b39ea181e20
yTakkar <www.shtakkar@gmail.com>
2018-02-28 12:45:22 +0530

    Made package less dependent on other packages

--- browser-env.js
+++ browser-env.js
@@ -3,37 +3,28 @@
 const
   fs = require('fs'),
   { success, error } = require('handy-log'),
-  { promisify } = require('util'),
-  { coroutine } = require('bluebird'),
+  parse = require('dotenv').parse,
   srcFile = './.env',
-  outputFile = './browser-env.js',
-  append = promisify(fs.appendFile),
-  exists = promisify(fs.exists),
-  deleteFile = promisify(fs.unlink)
+  outputFile = './browser-env.js'
 
 if (!fs.existsSync(srcFile)) {
   error('.Env file is missing!!')
   return
 }
 
-coroutine(function* (){
-  let
-    parse = require('dotenv').parse,
-    valuesFromFile = parse(fs.readFileSync(srcFile)),
-    result = [],
-    fileExists = yield exists(outputFile)
+let
+  valuesFromFile = parse(fs.readFileSync(srcFile)),
+  result = [],
+  fileExists = fs.existsSync(outputFile)
 
-  if (fileExists) {
-    deleteFile(outputFile)
-  }
+if (fileExists) {
+  fs.unlinkSync(outputFile)
+}
 
-  for (const elem in valuesFromFile) {
-    result[elem] = valuesFromFile[elem]
-  }
+for (let elem in valuesFromFile) {
+  result[elem] = valuesFromFile[elem]
+}
 
-  yield append(outputFile, `module.exports = ${JSON.stringify(valuesFromFile, null, 2)}`)
+fs.appendFileSync(outputFile, `module.exports = ${JSON.stringify(valuesFromFile, null, 2)}`)
 
-  success(`Successfully created a ${outputFile} file.`)
-
-})()
-
+success(`Successfully created a ${outputFile} file.`)
