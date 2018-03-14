259cb953d0d208f6fe051e9cf3443cc0fb3b8758
yTakkar <www.shtakkar@gmail.com>
2018-03-14 14:58:46 +0530

    Some changes in code as suggested by @maxbeatty

--- client-env.js
+++ client-env.js
@@ -3,28 +3,21 @@
 const
   fs = require('fs'),
   { success, error } = require('handy-log'),
-  parse = require('dotenv').parse,
-  srcFile = './.env',
+  { config } = require('dotenv'),
   outputFile = './env.js'
 
-if (!fs.existsSync(srcFile)) {
-  error('.Env file is missing!!')
+const { error: e, parsed } = config()
+if (e) {
+  error(e)
 } else {
 
   if (fs.existsSync(outputFile)) {
     fs.unlinkSync(outputFile)
   }
 
-  let
-    valuesFromFile = parse(fs.readFileSync(srcFile)),
-    result = []
-
-  for (let elem in valuesFromFile) {
-    result[elem] = valuesFromFile[elem]
-  }
-
-  fs.appendFileSync(outputFile, `module.exports = ${JSON.stringify(valuesFromFile, null, 2)}`)
+  fs.appendFileSync(outputFile, `module.exports = ${JSON.stringify(parsed, null, 2)}`)
 
   success(`Successfully created a ${outputFile} file.`)
 
 }
+
